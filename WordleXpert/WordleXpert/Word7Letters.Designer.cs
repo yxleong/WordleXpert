a6f9e2e2cf4acf36ce6dc731d68ad5e4fc8df6a6
stiyqr <stiyqr@gmail.com>
2024-12-03 01:31:55 +0800

    added Word7Letters user control

---
+++ WordleXpert/WordleXpert/Word7Letters.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WordleXpert
+{
+    partial class Word7Letters
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLetter1 = new System.Windows.Forms.TextBox();
+            this.txtLetter2 = new System.Windows.Forms.TextBox();
+            this.txtLetter3 = new System.Windows.Forms.TextBox();
+            this.txtLetter4 = new System.Windows.Forms.TextBox();
+            this.txtLetter5 = new System.Windows.Forms.TextBox();
+            this.txtLetter6 = new System.Windows.Forms.TextBox();
+            this.txtLetter7 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtLetter1
+            // 
+            this.txtLetter1.Enabled = false;
+            this.txtLetter1.Location = new System.Drawing.Point(28, 9);
+            this.txtLetter1.MaxLength = 1;
+            this.txtLetter1.Name = "txtLetter1";
+            this.txtLetter1.ReadOnly = true;
+            this.txtLetter1.Size = new System.Drawing.Size(24, 20);
+            this.txtLetter1.TabIndex = 1;
+            this.txtLetter1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtLetter1.TextChanged += new System.EventHandler(this.txtLetter_TextChanged);
+            this.txtLetter1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtLetter_KeyDown);
+            this.txtLetter1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLetter_KeyPress);
+            // 
+            // txtLetter2
+            // 
+            this.txtLetter2.Enabled = false;
+            this.txtLetter2.Location = new System.Drawing.Point(77, 9);
+            this.txtLetter2.MaxLength = 1;
+            this.txtLetter2.Name = "txtLetter2";
+            this.txtLetter2.ReadOnly = true;
+            this.txtLetter2.Size = new System.Drawing.Size(24, 20);
+            this.txtLetter2.TabIndex = 2;
+            this.txtLetter2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtLetter2.TextChanged += new System.EventHandler(this.txtLetter_TextChanged);
+            this.txtLetter2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtLetter_KeyDown);
+            this.txtLetter2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLetter_KeyPress);
+            // 
+            // txtLetter3
+            // 
+            this.txtLetter3.Enabled = false;
+            this.txtLetter3.Location = new System.Drawing.Point(124, 9);
+            this.txtLetter3.MaxLength = 1;
+            this.txtLetter3.Name = "txtLetter3";
+            this.txtLetter3.ReadOnly = true;
+            this.txtLetter3.Size = new System.Drawing.Size(24, 20);
+            this.txtLetter3.TabIndex = 3;
+            this.txtLetter3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtLetter3.TextChanged += new System.EventHandler(this.txtLetter_TextChanged);
+            this.txtLetter3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtLetter_KeyDown);
+            this.txtLetter3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLetter_KeyPress);
+            // 
+            // txtLetter4
+            // 
+            this.txtLetter4.Enabled = false;
+            this.txtLetter4.Location = new System.Drawing.Point(174, 9);
+            this.txtLetter4.MaxLength = 1;
+            this.txtLetter4.Name = "txtLetter4";
+            this.txtLetter4.ReadOnly = true;
+            this.txtLetter4.Size = new System.Drawing.Size(24, 20);
+            this.txtLetter4.TabIndex = 4;
+            this.txtLetter4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtLetter4.TextChanged += new System.EventHandler(this.txtLetter_TextChanged);
+            this.txtLetter4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtLetter_KeyDown);
+            this.txtLetter4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLetter_KeyPress);
+            // 
+            // txtLetter5
+            // 
+            this.txtLetter5.Enabled = false;
+            this.txtLetter5.Location = new System.Drawing.Point(224, 9);
+            this.txtLetter5.MaxLength = 1;
+            this.txtLetter5.Name = "txtLetter5";
+            this.txtLetter5.ReadOnly = true;
+            this.txtLetter5.Size = new System.Drawing.Size(24, 20);
+            this.txtLetter5.TabIndex = 5;
+            this.txtLetter5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtLetter5.TextChanged += new System.EventHandler(this.txtLetter_TextChanged);
+            this.txtLetter5.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtLetter_KeyDown);
+            this.txtLetter5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLetter_KeyPress);
+            // 
+            // txtLetter6
+            // 
+            this.txtLetter6.Enabled = false;
+            this.txtLetter6.Location = new System.Drawing.Point(275, 9);
+            this.txtLetter6.MaxLength = 1;
+            this.txtLetter6.Name = "txtLetter6";
+            this.txtLetter6.ReadOnly = true;
+            this.txtLetter6.Size = new System.Drawing.Size(24, 20);
+            this.txtLetter6.TabIndex = 6;
+            this.txtLetter6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtLetter6.TextChanged += new System.EventHandler(this.txtLetter_TextChanged);
+            this.txtLetter6.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtLetter_KeyDown);
+            this.txtLetter6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLetter_KeyPress);
+            // 
+            // txtLetter7
+            // 
+            this.txtLetter7.Enabled = false;
+            this.txtLetter7.Location = new System.Drawing.Point(325, 9);
+            this.txtLetter7.MaxLength = 1;
+            this.txtLetter7.Name = "txtLetter7";
+            this.txtLetter7.ReadOnly = true;
+            this.txtLetter7.Size = new System.Drawing.Size(24, 20);
+            this.txtLetter7.TabIndex = 7;
+            this.txtLetter7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtLetter7.TextChanged += new System.EventHandler(this.txtLetter_TextChanged);
+            this.txtLetter7.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtLetter7_KeyDown);
+            this.txtLetter7.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLetter7_KeyPress);
+            // 
+            // Word7Letters
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtLetter7);
+            this.Controls.Add(this.txtLetter6);
+            this.Controls.Add(this.txtLetter5);
+            this.Controls.Add(this.txtLetter4);
+            this.Controls.Add(this.txtLetter3);
+            this.Controls.Add(this.txtLetter2);
+            this.Controls.Add(this.txtLetter1);
+            this.Name = "Word7Letters";
+            this.Size = new System.Drawing.Size(377, 38);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLetter1;
+        private System.Windows.Forms.TextBox txtLetter2;
+        private System.Windows.Forms.TextBox txtLetter3;
+        private System.Windows.Forms.TextBox txtLetter4;
+        private System.Windows.Forms.TextBox txtLetter5;
+        private System.Windows.Forms.TextBox txtLetter6;
+        private System.Windows.Forms.TextBox txtLetter7;
+    }
+}
---
+++ WordleXpert/WordleXpert/Word7Letters.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordleXpert
+{
+    public partial class Word7Letters : UserControl
+    {
+        private string _word = "";
+
+        public string Word { get { return _word; } }
+
+        public event EventHandler WordEntered;
+
+        protected virtual void OnWordEntered(EventArgs e)
+        {
+            UpdateCurrentWord();
+            WordEntered?.Invoke(this, e);
+        }
+
+        public Word7Letters()
+        {
+            InitializeComponent();
+
+            txtLetter1.BackColor = Color.White;
+            txtLetter2.BackColor = Color.White;
+            txtLetter3.BackColor = Color.White;
+            txtLetter4.BackColor = Color.White;
+            txtLetter5.BackColor = Color.White;
+            txtLetter6.BackColor = Color.White;
+            txtLetter7.BackColor = Color.White;
+        }
+
+        public void HandleLetterFocus()
+        {
+            int letterCount = _word.Length;
+
+            txtLetter1.ReadOnly = !(letterCount == 0);
+            txtLetter2.ReadOnly = !(letterCount == 1);
+            txtLetter3.ReadOnly = !(letterCount == 2);
+            txtLetter4.ReadOnly = !(letterCount == 3);
+            txtLetter5.ReadOnly = !(letterCount == 4);
+            txtLetter6.ReadOnly = !(letterCount == 5);
+            txtLetter7.ReadOnly = !(letterCount >= 6);
+
+            txtLetter1.Enabled = (letterCount == 0);
+            txtLetter2.Enabled = (letterCount == 1);
+            txtLetter3.Enabled = (letterCount == 2);
+            txtLetter4.Enabled = (letterCount == 3);
+            txtLetter5.Enabled = (letterCount == 4);
+            txtLetter6.Enabled = (letterCount == 5);
+            txtLetter7.Enabled = (letterCount >= 6);
+
+            switch (letterCount)
+            {
+                case 0: txtLetter1.Focus(); break;
+                case 1: txtLetter2.Focus(); break;
+                case 2: txtLetter3.Focus(); break;
+                case 3: txtLetter4.Focus(); break;
+                case 4: txtLetter5.Focus(); break;
+                case 5: txtLetter6.Focus(); break;
+                default: txtLetter7.Focus(); break;
+            }
+        }
+
+        private void HandleBackspace()
+        {
+            int letterCount = _word.Length;
+            letterCount--;
+
+            switch (letterCount)
+            {
+                case 0: txtLetter1.Text = ""; break;
+                case 1: txtLetter2.Text = ""; break;
+                case 2: txtLetter3.Text = ""; break;
+                case 3: txtLetter4.Text = ""; break;
+                case 4: txtLetter5.Text = ""; break;
+                case 5: txtLetter6.Text = ""; break;
+                default: txtLetter7.Text = ""; break;
+            }
+        }
+
+        private void UpdateCurrentWord()
+        {
+            _word = txtLetter1.Text + txtLetter2.Text + txtLetter3.Text + txtLetter4.Text + txtLetter5.Text + txtLetter6.Text + txtLetter7.Text;
+        }
+
+        private void txtLetter_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // only allow alphabets and backspace
+            e.Handled = !(char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Back);
+        }
+
+        private void txtLetter7_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // only allow alphabets, backspace, enter
+            e.Handled = !(char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Back || e.KeyChar == (char)Keys.Enter);
+        }
+
+        private void txtLetter_TextChanged(object sender, EventArgs e)
+        {
+            UpdateCurrentWord();
+            HandleLetterFocus();
+        }
+
+        private void txtLetter_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Back)
+            {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+
+                HandleBackspace();
+                UpdateCurrentWord();
+                HandleLetterFocus();
+            }
+        }
+
+        private void txtLetter7_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Back)
+            {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+
+                HandleBackspace();
+                UpdateCurrentWord();
+                HandleLetterFocus();
+            }
+            else if (e.KeyCode == Keys.Enter)
+            {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+
+                if (txtLetter7.Text.Length == 0) return;
+
+                UpdateCurrentWord();
+                OnWordEntered(EventArgs.Empty);
+            }
+            else if (txtLetter7.Text.Length > 0)
+            {
+                e.SuppressKeyPress = true;
+            }
+        }
+    }
+}
