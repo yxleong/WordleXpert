1b1837549ae00b5cb978fab1181b459ea0c5048a
stiyqr <stiyqr@gmail.com>
2024-12-03 02:07:02 +0800

    added 8-letter game form

--- WordleXpert/WordleXpert/FormStartMenu.cs
+++ WordleXpert/WordleXpert/FormStartMenu.cs
@@ -23,6 +23,7 @@ namespace WordleXpert
 
             if (Program.WordLength == 6) formGame = new Form6Letters();
             else if (Program.WordLength == 7) formGame = new Form7Letters();
+            else if (Program.WordLength == 8) formGame = new Form8Letters();
             else formGame = new Form5Letters();
 
             formGame.Location = this.Location;
