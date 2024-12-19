97970ee2eafc683cef93be96166375264fd60e74
yxleong <yx0829leong@gmail.com>
2024-12-19 21:37:20 +0800

    update img file
    forcely

--- WordleXpert/WordleXpert/FormRegister.Designer.cs
+++ WordleXpert/WordleXpert/FormRegister.Designer.cs
@@ -38,16 +38,16 @@
             // pwdText2
             // 
             this.pwdText2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.pwdText2.Location = new System.Drawing.Point(160, 204);
+            this.pwdText2.Location = new System.Drawing.Point(238, 170);
             this.pwdText2.Name = "pwdText2";
-            this.pwdText2.Size = new System.Drawing.Size(262, 29);
+            this.pwdText2.Size = new System.Drawing.Size(184, 29);
             this.pwdText2.TabIndex = 0;
             this.pwdText2.TextChanged += new System.EventHandler(this.pwdText2_TextChanged);
             // 
             // usrText
             // 
             this.usrText.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.usrText.Location = new System.Drawing.Point(160, 101);
+            this.usrText.Location = new System.Drawing.Point(160, 100);
             this.usrText.Name = "usrText";
             this.usrText.Size = new System.Drawing.Size(262, 29);
             this.usrText.TabIndex = 0;
@@ -55,7 +55,7 @@
             // pwdText
             // 
             this.pwdText.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.pwdText.Location = new System.Drawing.Point(160, 141);
+            this.pwdText.Location = new System.Drawing.Point(160, 135);
             this.pwdText.Name = "pwdText";
             this.pwdText.Size = new System.Drawing.Size(262, 29);
             this.pwdText.TabIndex = 0;
@@ -64,9 +64,9 @@
             // buttonRegister
             // 
             this.buttonRegister.BackgroundImage = global::WordleXpert.Properties.Resources.btn_Register;
-            this.buttonRegister.Location = new System.Drawing.Point(160, 265);
+            this.buttonRegister.Location = new System.Drawing.Point(160, 224);
             this.buttonRegister.Name = "buttonRegister";
-            this.buttonRegister.Size = new System.Drawing.Size(106, 37);
+            this.buttonRegister.Size = new System.Drawing.Size(106, 40);
             this.buttonRegister.TabIndex = 1;
             this.buttonRegister.UseVisualStyleBackColor = true;
             this.buttonRegister.Click += new System.EventHandler(this.btnReg_Click);
@@ -75,8 +75,8 @@
             // 
             this.lblReg.AutoSize = true;
             this.lblReg.BackColor = System.Drawing.SystemColors.Window;
-            this.lblReg.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.lblReg.Location = new System.Drawing.Point(164, 308);
+            this.lblReg.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblReg.Location = new System.Drawing.Point(164, 271);
             this.lblReg.Name = "lblReg";
             this.lblReg.Size = new System.Drawing.Size(42, 16);
             this.lblReg.TabIndex = 2;
@@ -84,10 +84,10 @@
             // 
             // FormRegister
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::WordleXpert.Properties.Resources.bg_Register;
-            this.ClientSize = new System.Drawing.Size(443, 351);
+            this.ClientSize = new System.Drawing.Size(452, 308);
             this.Controls.Add(this.lblReg);
             this.Controls.Add(this.buttonRegister);
             this.Controls.Add(this.usrText);
